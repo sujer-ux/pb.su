60f148ca8682cc87b595a1c65debf008085344d0
sujer-ux <mat123matvei.123@gmail.com>
2021-12-27 20:46:32 +0500

    slick&nav

---
+++ JS/slick.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.slider').slick({
+          variableWidth: true,
+          autoplay: true,
+          autoplaySpeed: 5000,
+          speed: 1000,
+          centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 3,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ]
+    });
+});
